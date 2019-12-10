e030da1973e5e413ba624fb2cd0dabfbb81d6fe4
Evan Grabenstein <evangdesigns@gmail.com>
2019-12-10 00:27:42 -0600

    added skills data wit font awesome icons

--- src/javascripts/components/technologies/tech.js
+++ src/javascripts/components/technologies/tech.js
@@ -1,29 +1,22 @@
+import skillsData from '../../helpers/data/skillsData';
 import utl from '../../helpers/utilities';
 import './tech.scss';
 
 const printTech = () => {
-  let domString = '';
-  domString += `
-  <h2>Technology & Skills</h2>
-  <div id="skillsChart"></div>
-  <div class="meter">
-  <span class="web" style="height: 100px"></span>
-  <p><i class="fab fa-html5"></i></p>
-  </div>
-    <p>Adobe CC/Photoshop/Illustrator/InDesign</p>
-    <p>Microsoft Office/Word, Excel, Power Point</p>
-    <p>Multimedia/Web/GUI Design</p>
-    <p>Event Coordination, Planning & Execution</p>
-    <p>Social Media Management</p>
-    <p>Web Design HTML/CSS/Javascript</p>
-    <p>PC/Mac Proficiency</p>
-    <p>Photography & Photo Editing</p>
-    <p>Proofreading/Editing Design</p>
-    <p>Business Communication</p>
-    <p>Marketing Analytics</p>
-    <p>Copyrighting</p>
-  `;
-  utl.printToDom('tech', domString);
+  let domString = `<h2>Technology & Skills</h2>
+  <div class="d-flex justify-content-center align-items-baseline">`;
+  skillsData.getSkills()
+    .then((skills) => {
+      skills.forEach((s) => {
+        domString += `<div class="meter">
+        <span style="height:${s.proficiency}%;"></span>
+        <p><i class="${s.icon} fa-2x"></i></p>
+        </div>`;
+      });
+      domString += '</div>';
+      utl.printToDom('tech', domString);
+    })
+    .catch((error) => console.error(error));
 };
 
 export default { printTech };
