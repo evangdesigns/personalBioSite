bcf1eed27224354c20c06d76bb24be2cac5a91b3
Evan Grabenstein <evangdesigns@gmail.com>
2019-12-21 14:17:59 -0600

    implemented images

--- src/javascripts/components/bio/bio.js
+++ src/javascripts/components/bio/bio.js
@@ -3,9 +3,11 @@ import utl from '../../helpers/utilities';
 import './bio.scss';
 
 const printBio = () => {
-  const domString = `<div class="d-flex flex-wrap justify-content-center bioInfo">
-  <img src="../../../images/headshot.jpg" alt="Evan Grabenstein headshot" />
-  <p class="text-center">I am a highly driven, Graphic Artist and Marketeer with over 10 years of continuous achievement in marketing and graphic design. I am seeking ever-increasing challenges in creative fields, aiming to exploit my extensive expertise in the fields of software design and development.</p>
+  const domString = `<div class="d-flex justify-content-center align-middle bioInfo">
+  <div>
+  <img src="https://github.com/evangdesigns/personalBioSite/blob/master/src/images/headshot.jpg?raw=true" alt="Evan Grabenstein headshot" />
+  </div>
+  <p class="align-middle">I am a highly driven, Graphic Artist and Marketeer with over 10 years of continuous achievement in marketing and graphic design. I am seeking ever-increasing challenges in creative fields, aiming to exploit my extensive expertise in the fields of software design and development.</p>
   </div>`;
   utl.printToDom('bio', domString);
 };
